a100af95becfa9d828dc9c766ce70186d1beea73
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-05-12 21:59:30 +0530

    Greetings_name

---
+++ Python_Master_Class_Projects/greetings.py
@@ -0,0 +1,7 @@
+name='Tim'
+print('Welcome',name,'!') #Welcome Tim !
+name='Tim Cook'
+company='Apple'
+print('Let\'s Welcome',name,"-The CEO of",company)
+take_name=input('Enter your name: ')
+print('Hello',take_name)
