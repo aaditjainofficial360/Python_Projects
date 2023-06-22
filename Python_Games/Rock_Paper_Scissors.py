32fee88cc901596e22d1536c4e29b75033eef74f
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2023-06-22 18:16:16 +0530

    Update Rock_Paper_Scissors.py

--- Python_Games/Rock_Paper_Scissors.py
+++ Python_Games/Rock_Paper_Scissors.py
@@ -9,43 +9,93 @@ while choose.lower()=='yes':
     user1=input('Enter your choice(Rock/Paper/Scissors): ')
     time.sleep(6)
     user2 = input('Enter your choice(Rock/Paper/Scissors): ')
+    user1_points_this_round= 0
+    user2_points_this_round= 0
     if user1.lower()=='rock':
         if user2.lower() not in ['paper','rock']:
+            user1_points_this_round+=1
             user1_points+=1
+            print()
+            print('^'*15)
             print('You won this round !')
+            print('^' * 15)
+            print()
         elif user2.lower()=='paper':
+            user2_points_this_round+=1
             user2_points+=1
+            print()
+            print('^' * 15)
             print('Computer Wins this round !')
+            print('^' * 15)
+            print()
         else:
             user1_points+=1
             user2_points+=1
+            user2_points_this_round+=1
+            user1_points_this_round+=1
+            print()
+            print('^' * 15)
             print('It\'s a Tie!')
+            print('^' * 15)
+            print()
 
     elif user1.lower()=='paper':
         if user2.lower() not in ['paper','scissors']:
+            user1_points_this_round+=1
             user1_points+=1
+            print()
+            print('^'*15)
             print('You won this round !')
+            print('^' * 15)
+            print()
         elif user2.lower()=='scissors':
+            user2_points_this_round+=1
             user2_points+=1
+            print()
+            print('^' * 15)
             print('Computer Wins this round !')
+            print('^' * 15)
+            print()
         else:
             user1_points+=1
             user2_points+=1
+            user2_points_this_round+=1
+            user1_points_this_round+=1
+            print()
+            print('^' * 15)
             print('It\'s a Tie!')
+            print('^' * 15)
+            print()
 
     else:
         if user2.lower() not in ['rock','scissors']:
             user1_points+=1
+            user1_points_this_round+=1
+            print()
+            print('^' * 15)
             print('You won this round !')
+            print('^' * 15)
+            print()
         elif user2.lower()=='rock':
             user2_points+=1
+            user2_points_this_round+=1
+            print()
+            print('^' * 15)
             print('Computer Wins this round !')
+            print('^' * 15)
+            print()
         else:
             user1_points+=1
             user2_points+=1
+            user1_points_this_round+=1
+            user2_points_this_round+=1
+            print()
+            print('^' * 15)
             print('It\'s a Tie!')
-    print(f'Your Score after this round : {user1_points}')
-    print(f'Computer\'s Score after this round : {user2_points}')
+            print('^' * 15)
+            print()
+    print(f'Your Score after this round : {user1_points_this_round}')
+    print(f'Computer\'s Score after this round : {user2_points_this_round}')
     choose=input('Do you want to continue playing this game(Yes/No): ')
     time.sleep(6)
 print()
