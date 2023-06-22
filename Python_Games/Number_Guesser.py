5c6e1c4e77997664a941201ab49da59858ea1a59
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2023-06-22 17:20:41 +0530

    Guess Game

---
+++ Python_Games/Number_Guesser.py
@@ -0,0 +1,16 @@
+import random
+
+print('*'*50)
+print('Welcome to Guessing Game')
+print('*'*50)
+start=int(input('Enter the starting value: '))
+end=int(input('Enter the ending value: '))
+if start<=end:
+    num=int(input(f'Enter any number between {start} and {end}: '))
+    random_num=random.randint(start,end)
+    if random_num==num:
+        print('You have rightly guessed! Congratulations!')
+    else:
+        print('Bad Luck!')
+else:
+    print('Starting value should be lesser than ending value')
