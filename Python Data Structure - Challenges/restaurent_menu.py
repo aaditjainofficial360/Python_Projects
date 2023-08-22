c1b520beb6e01f2a61533a2cc8aaa3afc0903349
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-08-22 22:37:59 +0530

    Create restaurent_menu.py

---
+++ Python Data Structure - Challenges/restaurent_menu.py
@@ -0,0 +1,14 @@
+
+menu={}
+
+starters=['Dhokla','Samosa','Idli Sambhar','Aloo Chat','Dahi Bhalla','Aloo Tikki','Matra Khulcha']
+salads=['Green Salad','Caeser Salad','Brocolli Corn Salad','White Pasta Corn Salad','Sprouts Salad']
+entree=['Blueberry Mojito','Lime Soda','Strawberry Smoothie','Pan Mojito','Peach Mojito']
+deserts=['Gulab Jamun','Kaju Katli','Jaleebi','Suji Halwa','Gajar Halwa','Rasmalai']
+
+menu['entree']=entree
+menu['salads']=salads
+menu['starters']=starters
+menu['deserts']=deserts
+
+print(menu)
