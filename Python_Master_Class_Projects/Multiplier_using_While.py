1582d94723e6018be4fcc6ee7a775d35ad2f78d4
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2023-10-05 00:06:05 +0530

    Create Multiplier_using_While.py

---
+++ Python_Master_Class_Projects/Multiplier_using_While.py
@@ -0,0 +1,9 @@
+print('*****************MULTIPLIER USING WHILE LOOP*******************')
+n=int(input("Enter any number: "))
+counter=1  
+while counter<=10:
+    print(f'{n} X {counter} = {counter*n}')
+    counter+=1 
+    
+print()
+print('Operation Completed!')
