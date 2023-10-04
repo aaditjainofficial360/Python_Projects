2d553149111fd2765a0507d2b32b7c253f1a1ec1
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2023-10-05 00:05:40 +0530

    Update Multiplier_using_for.py

--- Python_Master_Class_Projects/Multiplier_using_for.py
+++ Python_Master_Class_Projects/Multiplier_using_for.py
@@ -2,3 +2,5 @@ print('*****************MULTIPLIER*******************')
 n=int(input("Enter any number: "))
 for num in range(1,11):
     print(f'{n} X {num} = {num*n}')
+print()
+print('Operation Completed!')
